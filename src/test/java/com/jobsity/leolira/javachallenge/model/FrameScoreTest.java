cb9027f8a07c360cb86601d73e3e3aba9d2147e6
Leonardo Lira <cadlira@gmail.com>
2022-06-19 11:54:36 -0300

    Add mock to FrameScore test and update gradle.build

---
+++ src/test/java/com/jobsity/leolira/javachallenge/model/FrameScoreTest.java
@@ -0,0 +1,30 @@
+package com.jobsity.leolira.javachallenge.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class FrameScoreTest {
+
+	@Test
+	public void itShouldGetsTheFrameRound() {
+		FrameScore frameScore = new FrameScore(1);
+		
+		assertThat(frameScore.getFrameRound()).isEqualTo(1);
+	}
+	
+	@Test
+	public void itShouldAddAPlayerScore(@Mock PlayerScore playerScore) {
+		FrameScore frameScore = new FrameScore(1);
+		frameScore.addPlayerScore(playerScore);
+		
+		assertNotNull(frameScore.getPlayerScores());
+		assertThat(frameScore.getPlayerScores().size()).isEqualTo(1);
+		assertThat(frameScore.getPlayerScores().get(0)).isEqualTo(playerScore);
+	}
+}
