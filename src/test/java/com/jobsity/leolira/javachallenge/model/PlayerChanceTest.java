288a4b9bacc17a5d5d51d887b39179b2a91a5cee
Leonardo Lira <cadlira@gmail.com>
2022-06-19 13:25:09 -0300

    Remove unnecessary annotation

--- src/test/java/com/jobsity/leolira/javachallenge/model/PlayerChanceTest.java
+++ src/test/java/com/jobsity/leolira/javachallenge/model/PlayerChanceTest.java
@@ -6,10 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class PlayerChanceTest {
 
 	@Test
